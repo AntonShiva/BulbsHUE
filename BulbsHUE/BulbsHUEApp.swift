4a4db532c176a4164eefff058d7797c7b809c413
AntonShiva <ryashinanthon@yandex.ru>
2025-07-25 12:34:23 +0300

    Initial Commit

---
+++ BulbsHUE/BulbsHUEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulbsHUEApp.swift
+//  BulbsHUE
+//
+//  Created by Anton Reasin on 25.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct BulbsHUEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
