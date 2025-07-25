7c6fed90ed723f40bec757c3ef6eed83b1b1ebab
AntonShiva <ryashinanthon@yandex.ru>
2025-07-25 18:37:07 +0300

    Добавил кастомный шрифт и создал кнопку таббара.(добавил поддержку динамических размеров)

--- BulbsHUE/TabBar/TabBarButton.swift
+++ BulbsHUE/TabBar/TabBarButton.swift
@@ -8,47 +8,67 @@
 import SwiftUI
 
 //
-//struct TabBarButton: View {
-//    let activeIcon: Image
-//    let inactiveIcon: Image
-//    let title: String
-////    let route: Router
-////    @Binding var currentView: Router
-//    
-//    // Colors from Figma
-//    private let activeColor = Color(red: 0.33, green: 0.47, blue: 1) // Active color
-//    private let inactiveColor = Color(red: 0.41, green: 0.46, blue: 0.65) // Inactive color
-//    
-//    private var isSelected: Bool {
-////        currentView == route
-//        true
-//    }
-//    
-//    var body: some View {
-//        Button {
-////            currentView = route
-//        } label: {
-//            VStack(spacing: 2) {
-//                // Icon
-//                (isSelected ? activeIcon : inactiveIcon)
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .frame(width: 48, height: 48)
-//                
-//                // Title
-//                Text(title)
-//                    .font(.custom("Unbounded", size: 10))
-//                    .fontWeight(.regular)
-//                    .foregroundColor(isSelected ? activeColor : inactiveColor)
-//                    .opacity(isSelected ? 1.0 : 0.8)
-//                    .kerning(2.0) // 20% letter spacing approximation
-//                    .multilineTextAlignment(.center)
+struct TabBarButton: View {
+    let image: String
+   
+    let title: String
+//    let route: Router
+//    @Binding var currentView: Router
+    
+    // Colors from Figma
+    private let activeColor = Color(red: 0.79, green: 1, blue: 1) // Active color
+    private let inactiveColor = Color(red: 0.41, green: 0.46, blue: 0.65) // Inactive color
+    
+    private var isSelected: Bool {
+//        currentView == route
+        true
+    }
+    
+    var body: some View {
+        Button {
+//            currentView = route
+        } label: {
+            VStack(spacing: 9) {
+                // Icon
+               Image(image)
+                    .resizable()
+                    .renderingMode(.template)
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(isSelected ? activeColor : inactiveColor)
+                    .adaptiveFrame(width: 40, height: 40)
+                
+                // Title
+                Text(title)
+                    .font(Font.custom("DMSans-9ptRegular_Light", size: 10))
+                  .kerning(2.2)
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(isSelected ? activeColor : inactiveColor)
+                    .opacity(isSelected ? 1.0 : 0.8)
+                    .textCase(.uppercase)
+                    
 //                    .offset(x: title == "MIXER" ? 4 : 0)
-//            }
-//        }
-//        .buttonStyle(PlainButtonStyle())
-//    }
-//}
+            }
+        }
+        .buttonStyle(PlainButtonStyle())
+        .onAppear {
+              //  проверка шрифтов 
+              for family in UIFont.familyNames {
+                  print("== \(family)")
+                  for name in UIFont.fontNames(forFamilyName: family) {
+                      print("   - \(name)")
+                  }
+              }
+          }
+    }
+}
+#Preview {
+    ZStack {
+        BG()
+        TabBarButton(image: "envir", title: "environment")
+    }
+    .compare(with: URL(string: "https://www.figma.com/design/9yYMU69BSxasCD4lBnOtet/Bulbs_HUE--Copy-?node-id=2002-3&m=dev")!)
+    .environment(\.figmaAccessToken, "YOUR_FIGMA_TOKEN")
+}
 
 //#Preview {
 //    struct PreviewWrapper: View {
