5ed9748403849c14f93ee6d210982969322b9181
AntonShiva <ryashinanthon@yandex.ru>
2025-07-27 13:11:21 +0300

    Создал кастомную кнопку Add с возможностью переиспользования в проекте.

---
+++ BulbsHUE/Core/Views/Buttoms/AddButton.swift
@@ -0,0 +1,49 @@
+//
+//  AddBulbButton.swift
+//  BulbsHUE
+//
+//  Created by Anton Reasin on 26.07.2025.
+//
+
+import SwiftUI
+
+struct AddButton: View {
+    var text: String
+    var width: CGFloat
+    var height: CGFloat
+    var action: () -> Void
+    
+      var body: some View {
+          Button {
+              action()
+          } label: {
+              ZStack() {
+                  BGPlusButton()
+                      .adaptiveFrame(width: width, height: height)
+                  
+                  Text(text)
+                      .font(Font.custom("DM Sans", size: 16.5) .weight(.medium))
+                    .kerning(6)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 0.79, green: 1, blue: 1))
+                    .textCase(.uppercase)
+                    .adaptiveOffset(x: 20,y: -1.5)
+                    .blur(radius: 0.5)
+              }
+          }
+          .buttonStyle(PlainButtonStyle())
+       }
+    }
+
+#Preview {
+    ZStack {
+        BG()
+        
+        AddButton(text: "add bulb", width: 427, height: 295) {
+            
+        }
+    }
+    .compare(with: URL(string: "https://www.figma.com/design/9yYMU69BSxasCD4lBnOtet/Bulbs_HUE--Copy-?node-id=2002-3&t=w7kYvAzD6FTnifyZ-4")!)
+    .environment(\.figmaAccessToken, "figd_0tuspWW6vlV9tTm5dGXG002n2yoohRRd94dMxbXD")
+}
+
---
+++ BulbsHUE/Core/Views/Buttoms/BGCircleButton.swift
@@ -0,0 +1,19 @@
+//
+//  BGCircleButton.swift
+//  BulbsHUE
+//
+//  Created by Anton Reasin on 26.07.2025.
+//
+
+import SwiftUI
+
+struct BGCircleButton: View {
+    var body: some View {
+        Image("BGButton")
+            .resizable()
+    }
+}
+
+#Preview {
+    BGCircleButton()
+}
