76eed88a2ba142576acb45b8b58867e8f5392563
AntonShiva <ryashinanthon@yandex.ru>
2025-07-28 10:08:00 +0300

    Добавил фон для селектора.

---
+++ BulbsHUE/Modules/Environment/Views/SelectorTabEnviromentView.swift
@@ -0,0 +1,30 @@
+//
+//  SelectorTabEnviromentView.swift
+//  BulbsHUE
+//
+//  Created by Anton Reasin on 27.07.2025.
+//
+
+import SwiftUI
+
+struct SelectorTabEnviromentView: View {
+    var body: some View {
+        ZStack{
+            BGSelector()
+                .adaptiveFrame(width: 330, height: 102)
+           
+        }
+    }
+}
+
+#Preview {
+   ZStack {
+        BG()
+        
+       SelectorTabEnviromentView()
+    }
+    .compare(with: URL(string: "https://www.figma.com/design/9yYMU69BSxasCD4lBnOtet/Bulbs_HUE--Copy-?node-id=2002-3&t=w7kYvAzD6FTnifyZ-4")!)
+    .environment(\.figmaAccessToken, "figd_0tuspWW6vlV9tTm5dGXG002n2yoohRRd94dMxbXD")
+}
+
+
