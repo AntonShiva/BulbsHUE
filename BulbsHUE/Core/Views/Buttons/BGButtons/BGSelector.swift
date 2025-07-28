4a8d62a03107f4470ed8a104273e228b5bf72874
AntonShiva <ryashinanthon@yandex.ru>
2025-07-28 10:08:00 +0300

    Добавил фон для селектора.

---
+++ BulbsHUE/Core/Views/Buttons/BGButtons/BGSelector.swift
@@ -0,0 +1,19 @@
+//
+//  BGSelector.swift
+//  BulbsHUE
+//
+//  Created by Anton Reasin on 27.07.2025.
+//
+
+import SwiftUI
+
+struct BGSelector: View {
+    var body: some View {
+        Image("BGSelector")
+            .resizable()
+    }
+}
+
+#Preview {
+    BGSelector()
+}
