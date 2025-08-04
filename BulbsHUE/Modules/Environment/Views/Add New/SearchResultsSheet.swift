c682ba0b74affa36b7325126b786ce7fa5ba003f
AntonShiva <ryashinanthon@yandex.ru>
2025-08-04 15:44:00 +0300

    создал SearchResultsShee (UI)

--- BulbsHUE/Modules/Environment/Views/Add New/SearchResultsSheet.swift
+++ BulbsHUE/Modules/Environment/Views/Add New/SearchResultsSheet.swift
@@ -9,14 +9,28 @@ import SwiftUI
 
 struct SearchResultsSheet: View {
     var body: some View {
-        UnevenRoundedRectangle(
-            topLeadingRadius: 35,
-            bottomLeadingRadius: 0,
-            bottomTrailingRadius: 0,
-            topTrailingRadius: 35
-        )
-        .fill(Color(red: 0.02, green: 0.09, blue: 0.13))
-        .adaptiveFrame(width: 375, height: 342)
+        ZStack {
+            UnevenRoundedRectangle(
+                topLeadingRadius: 35,
+                bottomLeadingRadius: 0,
+                bottomTrailingRadius: 0,
+                topTrailingRadius: 35
+            )
+            .fill(Color(red: 0.02, green: 0.09, blue: 0.13))
+            .adaptiveFrame(width: 375, height: 342)
+            
+            Text("search results")
+              .font(Font.custom("DMSans-Light", size: 14))
+              .kerning(2.8)
+              .multilineTextAlignment(.center)
+              .foregroundColor(Color(red: 0.79, green: 1, blue: 1))
+              .textCase(.uppercase)
+              .adaptiveOffset(y: -130)
+            
+            TupeCell(text: "Bulb name", image: "lightBulb", width: 32, height: 32)
+                .adaptiveOffset(y: -70)
+        }
+        
     }
 }
 
